a6abf2f060e02f28b20839ee37eadeb95807ff71
Cole Hlava <cole.hlava@gmail.com>
2020-04-05 21:17:01 -0700

    Experimenting with forms for inputting new stock.

--- index.php
+++ index.php
@@ -19,6 +19,13 @@
     }
 ?>
 <br>
+<form action="/action_page.php">
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname"><br><br>
+    <label for="lname">Last name:</label>
+    <input type="text" id="lname" name="lname"><br><br>
+    <input type="submit" value="Submit">
+</form>
 </div>
 </body>
 </html>
