ceecf1b0f5fcb8a64f9f48a4a89dfbe28f760177
Cole Hlava <cole.hlava@gmail.com>
2020-04-05 21:10:33 -0700

    Minor changes to php file.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <body>
 <div align="center" style="background-color:lightgreen">
+<br>
 <h1>Stock Prices</h1>
 <?php
     $stockList = file('watchlist.txt');
@@ -11,7 +12,6 @@
         echo $stock;
         echo ": $";
         $filename = $stock . ".price";
-        $filename = trim($filename);
         $contents = file_get_contents($filename);
         $contents = trim($contents);
         echo $contents;
